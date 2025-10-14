a0b1e8864643e915bf089b339163a7f98468bd6f
mokshamani <mokshamanibsc24@rvu.edu.in>
2025-10-14 10:10:28 +0530

    Delete SimpleCalculator.java

--- SimpleCalculator.java
+++
@@ -1,9 +0,0 @@
-public class SimpleCalculator {
-public static void main(String[] args) {
-int a = 20, b = 10;
-System.out.println("Addition: " + (a + b));
-System.out.println("Subtraction: " + (a - b));
-System.out.println("Multiplication: " + (a * b));
-System.out.println("Division: " + (a / b));
-}
-}
